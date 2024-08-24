39cd9d4293dd6f1c594c7230a5e6e3bb07de1cd4
Thozs <161093262+Thozs@users.noreply.github.com>
2024-08-24 15:01:34 -0300

    Fábrica e media de notas

---
+++ Aula01C#Fábrica/Aula01C#Fábrica/Media2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula01C_Fábrica
+{
+    public  class Media2
+    {
+        public void calculomedia()
+        {
+            Console.WriteLine("informe a nota 1");
+            double nota1 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("informe a nota2");
+            double nota2 = Convert.ToDouble(Console.ReadLine());
+
+            double media = (nota1 + nota2) / 2;
+
+            Console.WriteLine(media); //debugar
+
+            if (media >= 7)
+            {
+                Console.WriteLine("Aprovado");
+            }
+            else
+            {
+                Console.WriteLine("Reprovado");
+            }
+        }
+    }
+}
